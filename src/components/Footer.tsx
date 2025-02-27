f666b9442697ecafa85fa7a5e6c04b54b414d4a5
Victor Callegari <agro.callegari@hotmail.com>
2025-02-26 22:40:58 -0300

    alteração no nome do footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
     return (
       <footer className="bg-gray-200 text-center p-4 mt-8">
         <p className="text-gray-700">
-          Desenvolvido por <span className="font-bold">CampoTech</span>
+          Desenvolvido por <span className="font-bold">VICTOR CALLEGARI&reg;</span>
         </p>
         <p className="text-gray-500 text-sm">© {new Date().getFullYear()} Todos os direitos reservados.</p>
       </footer>
